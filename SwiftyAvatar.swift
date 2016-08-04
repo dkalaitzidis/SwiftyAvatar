83300a4bdefcb3067eb47b1b71867c8ed111982a
Dimitrios Kalaitzidis <kalaitzidis34@gmail.com>
2016-08-04 16:45:57 +0300

    Initial Commit

---
+++ SwiftyAvatar.swift
@@ -0,0 +1,49 @@
+//
+//  SwiftyAvatar.swift
+//  SwiftyAvatar
+//
+//  Created by Dimitrios Kalaitzidis on 04/08/16.
+//  Copyright © 2016 Dimitrios Kalaitzidis. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class SwiftyAvatar: UIImageView {
+
+    @IBInspectable var roundness: CGFloat = 0.0 {
+        didSet{
+            setup()
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet{
+            setup()
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor = UIColor.clearColor() {
+        didSet{
+            setup()
+        }
+    }
+
+    override var bounds: CGRect {
+        get {
+            return super.bounds
+        }
+        set {
+            super.bounds = newValue
+            setNeedsLayout()
+        }
+    }
+
+    
+    func setup() {
+        super.layoutSubviews()
+        layer.cornerRadius = bounds.width / roundness
+        layer.borderWidth = borderWidth
+        layer.borderColor = borderColor.CGColor
+        clipsToBounds = true
+    }
+}
