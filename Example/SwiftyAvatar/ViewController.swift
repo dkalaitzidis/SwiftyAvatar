7a41d68ce107b6a8aeae59fd99e831ffd60df134
Rishabh Tayal <rtayal11@gmail.com>
2016-08-08 09:17:39 -0500

    added pods project structure, added example project

---
+++ Example/SwiftyAvatar/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftyAvatar
+//
+//  Created by Rishabh Tayal on 08/08/2016.
+//  Copyright (c) 2016 Rishabh Tayal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
